026d2332c29f1eb29a98d7e3ab6aaa83101c289d
david1999erazo <david.erazo2@correo.icesi.edu.co>
2020-03-06 23:05:42 -0500

    Add show map

--- app/src/main/java/appmoviles/com/googlemapsapp/MapsActivity.java
+++ app/src/main/java/appmoviles/com/googlemapsapp/MapsActivity.java
@@ -1,7 +1,12 @@
 package appmoviles.com.googlemapsapp;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,19 +16,44 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.security.PublicKey;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int REQUEST_CODE = 11;
+
     private GoogleMap mMap;
+    private LocationManager locationManager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
+
+
+        ActivityCompat.requestPermissions(this, new String []{
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        }, REQUEST_CODE);
+
+        /*
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 11);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 1, locationListener);
+
+        }*/
     }
+
 
 
     /**
